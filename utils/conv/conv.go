04d09478e2e27be26ff2802a3b48712c45def25d
xylo <m.aditya3232@gmail.com>
2026-05-26 10:36:57 +0700

    add util conv

---
+++ utils/conv/conv.go
@@ -0,0 +1,21 @@
+package conv
+
+import (
+	"strconv"
+)
+
+func StringToInt64(s string) (int64, error) {
+	newData, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return newData, nil
+}
+
+func Int64PointerToInt64(num *int64) int64 {
+	if num != nil {
+		return *num
+	}
+	return 0
+}
